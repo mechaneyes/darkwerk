ce3b38af1344e47f03d829097013512619718254
Ray Weitzenberg <ray@mechaneyes.com>
2025-01-28 15:59:30 -0500

    darkerk -> darkwerk

--- src/app/(pages)/geist/layout.tsx
+++ src/app/(pages)/geist/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "darkerk",
+  title: "darkwerk",
   description: "Ain't Nothin' Goin' on but the Ghosts",
   icons: {
     icon: [
